471685de8da6bbc3911a60235efe5123ee509f23
xin <xin@ns.mqcache.net>
2015-04-04 17:48:01 +0800

    common.hrl

--- include/common.hrl
+++ include/common.hrl
@@ -11,9 +11,25 @@
 
 -define(DEBUG(Message), common:debug(Message)).
 
+-define(GV(Key), common:get_env(Key)).
+
 -define(IsDEBUG, true).
 
 -record(request, {req, uid, platid=0, token, ip, port, code=0, data="", res=[], ud=[], tlog_params=[], oprdno=undefined, cid=0}).
 
--define(DEF_PORT,    2222).
 
+-define(POOL_NAME, blog_pool).
+-define(MYSQL_POOL_HOST, mysql_pool_host).
+-define(MYSQL_POOL_SIZE, mysql_pool_size).
+-define(MYSQL_POOL_PORT, mysql_pool_port).
+-define(MYSQL_USER, mysql_user).
+-define(MYSQL_PWD, mysql_pwd).
+-define(MYSQL_DBNAME, mysql_dbname).
+
+
+-record(user, {id, username, email, password, date}).
+-record(blog_content, {id, content, user_id, title, date}).
+
+
+
+-define(PAGESIZE, 10).
